126c8cf5e3b7ecb78f4070b92e904fc577fed176
oddmmar <oddmmar@gmail.com>
2023-10-05 07:31:44 +0200

    adds file existence checking

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# autotranscode
+# Auto Transcode
+
+## House Formats
+
+```
+IMX 30
+IMX 50
+XDCAM HD 50
+AVC-Intra 100
+```
--- tomp4bash.sh
+++ tomp4bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-searchDir=./SRC
-dstDir=./DST
-logDir=./LOG
+searchDir="/Volumes/DATA/media/Transcoding/SRC"
+dstDir="/Volumes/DATA/media/Transcoding/DST"
+logDir="/Volumes/DATA/media/Transcoding/LOG"
 # default extension, but assignable with -e flag
 ext="mov"
 
@@ -44,15 +44,38 @@ while getopts e:s: flag
     done
 fi
 
-
+# recursive search for every file in tehe given directory 
 for file in $(find "$searchDir" -type f); do
-    # process file only if it has the given extension
-    if [ "${file##*.}" == "$ext" ]; then
-        # Check if the filename contains "raw," "RAW," or "Raw" (case-insensitive)
-        if echo "$file" | grep -iq 'raw'; then
-            printToConsole "Info:" "Processing file ->\t$file"
-        else
-            printToConsole "Warning:" "Unprocessed file ->\t$file"
+    # check file existsnce
+    if [  -e "$file" ]; then 
+        # process file only if it has the given extension
+        if [ "${file##*.}" == "$ext" ]; then
+            # Check if the filename contains "raw," "RAW," or "Raw" (case-insensitive)
+            if echo "$file" | grep -iq 'raw'; then
+                printToConsole "Info:" "Processing file ->\t$file"
+                printToLog "Info:" "Processing file ->\t$file"
+                # get file information about the number of audio streams
+                fileInfo=$(ffprobe -hide_banner -v error -select_streams a -show_entries stream=index -of csv=p=0 $file)
+                # get length
+                res=${#fileInfo}
+                # Stream #0.1 -> #0.1 [channel: 0 -> 0]
+                # Stream #0.2 -> #0.1 [channel: 0 -> 1]
+                if [ $res == 3 ]; then
+                    echo "Two audio streams with one channel -> 0:1:0, 0:2:0"
+                    # ffmbc -i "$file" -target imx30 -map_audio_channel '0:1:0:0:1:0' -map_audio_channel '0:2:0:0:1:1' -y "$dstDir/$(date +%Y%m%d-%H.%M)_$actualName-RAW.mxf"
+                fi
+                # Stream #0.1 -> #0.1 [channel: 0 -> 0]
+                # Stream #0.1 -> #0.1 [channel: 0 -> 1]
+                if [ $res == 1 ]; then
+                    echo "One audio stream with two channel -> 0:1:0, 0:1:1"
+                    #  ffmbc -i "$file" -target imx30 -map_audio_channel '0:1:0:0:1:0' -map_audio_channel '0:1:1:0:1:1' -y "$dstDir/$(date +%Y%m%d-%H.%M)_$actualName-RAW.mxf"
+                fi
+            else
+                printToConsole "Warning:" "Unprocessed file ->\t$file"
+                printToLog "Warning:" "Unprocessed file ->\t$file"
+            fi
         fi
+    else 
+        printToConsole "Error:" "File not found ->\t$file"
     fi
 done
