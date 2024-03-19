abf805de8243edd5a62992bd4be965ee6fb68ad7
oddmmar <oddmmar@gmail.com>
2024-03-19 23:44:15 +0200

    file organisation

--- createcontefile.sh
+++
@@ -1,8 +0,0 @@
-sourcePath="/Volumes/DATA/media/transcoding/SRC"
-touch contentfiile.txt
-
-find "$sourcePath" -type f -print0 | while IFS= read -r -d $'\0' file; do
-    echo "$file" >>contentfiile.txt
-done
-
-# $ find "AAA TO BE ARCHIVED 2 AAA" -type f -name "*raw*" -print0 | xargs -0 ls -l > /media/wctech/internal/500SSD/Transcoding/raw2.txt
